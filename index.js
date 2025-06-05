be5fe4a96d68c25afbe3ceb02248a1749007be75
Udit Pratap Singh <153190852+uditsinghs@users.noreply.github.com>
2025-06-05 07:39:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
     origin: [
       "http://localhost:5173", // for local testing
       process.env.PORTFOLIO_URL,
-      process.env.DASHBOARD_URL,
+      process.env.DASHBOARD_URL || "https://uditportfolio-dashboard.netlify.app",
     ],
     methods: ["GET", "DELETE", "PUT", "POST"],
     credentials: true,
